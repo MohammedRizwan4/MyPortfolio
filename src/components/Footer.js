46d4eadef82397be6594562fba8d1d73b593cdcb
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-08-07 18:49:15 +0530

    instagram icon deleted

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -51,16 +51,6 @@ function Footer() {
                 <FaLinkedinIn />
               </a>
             </li>
-            <li className="social-icons">
-              <a
-                href="https://www.instagram.com/rizwan"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiFillInstagram />
-              </a>
-            </li>
           </ul>
         </Col>
       </Row>
